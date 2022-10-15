e65012088602a6600449fc1b7f2987e530259456
Chirag Bhatia <cbhatia2002@gmail.com>
2022-10-15 22:20:47 +0530

    pytesseract

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
+import json
 from flask import Flask, jsonify, request
 from amazoncaptcha import AmazonCaptcha
+from PIL import Image
+import pytesseract
+import urllib.request
 import os
 
 
@@ -13,10 +17,23 @@ def home():
 def getCapt():
     data = request.args['link']
     result = data[1:-1]
-    captcha = AmazonCaptcha.fromlink(result)
-    solution = captcha.solve()
-    return jsonify({'text': solution})
 
+    f = open('cap.png','wb')
+    f.write(urllib.request.urlopen(result).read())
+    f.close()
+
+    # urllib.request.urlretrieve(result, "cap.jpg")
+    col = Image.open("cap.png")
+    gray = col.convert('L')
+    bw = gray.point(lambda x: 255 if x>=190 else 0, '1')
+    bw.save('result.png')
+
+    solution = pytesseract.image_to_string(Image.open('result.png'))
+    solution = solution.strip()
+    # captcha = AmazonCaptcha.fromlink(result)
+    # solution = captcha.solve()
+    solution = str(solution)
+    return jsonify({"text": solution})
 
 if __name__ == "__main__":
     app.run()
